19d3e09c1870a0ed9a3034624bdc397f791e06a3
Shosuke Doi <doiverson.canada@gmail.com>
2019-01-06 17:43:56 -0800

    had gulp file fixed and added HTML codes

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,36 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var bs = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const bs = require('browser-sync').create();
 
-// browser-sync
-// gulp.task('bs', function(){
-//     var bsOptions = {}
-//     bsOptions.files = ['./**/index.html', './css/**/*.css'];
-//     bsOptions.port  = 3000;
-//     bs.init(bsOptions)gul
-// });
+function sassBuild() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest('./css/'))
+        .pipe(bs.stream()) // Sassのコンパル後、BrowserSyncのリロードを行う
+}
 
-// タスク名と変数名は別々にしといた方が見やすいよ :)
-gulp.task("browserSync", function () {
+function browserSync(done) {
     bs.init({
         server: {
             baseDir: "./", // ルートとなるディレクトリを指定
         },
         port: 8080 // Portはこのオプションで変えれる
     });
-});
+    done();
+}
 
-// Sassコンパイルタスク
-gulp.task('sass', function(){
-    gulp.src('./scss/**/*.scss')
-        .pipe(plumber()) // ←ここが追加
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css/'))
-        .pipe(bs.stream()) // Sassのコンパル後、BrowserSyncのリロードを行う
-});
+function reload(done) {
+    bs.reload();
+    done();
+}
 
-gulp.task('html', function() {
-    gulp.src(['**/*.html'])
-        .pipe(bs.stream())
-});
+function watchAllFiles() {
+    gulp.watch('scss/**/*.scss', sassBuild);
+}
 
-// watchタスク(**/*.scss変更時に実行するタスク)
-// gulp.task('sass-watch', ['sass'], function(){
-//     var watcher = gulp.watch('./scss/**/*.scss', ['sass']);
-//     watcher.on('change', function(event) {
-//         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-//     });
-// });
+const watch = gulp.parallel(watchAllFiles, browserSync);
 
-// gulpのデフォルト動作としてsass-watchを実行
-// gulp.task('default', ['bs','sass-watch']);
-
-// gulpのデフォルトはwatchタスクを一緒に書いた方が楽かな
-gulp.task('default', ['browserSync'], function () {
-    gulp.watch('scss/**/*.scss',['sass']);
-    gulp.watch('**/*.html', ['html']);
-});
+exports.watch = watch;
+exports.default = watch;
