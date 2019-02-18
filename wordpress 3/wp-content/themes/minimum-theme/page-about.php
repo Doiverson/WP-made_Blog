c0e2e7042b4d3c3e88e4f97a27b9f3f0df04a90c
Shosuke Doi <doiverson.canada@gmail.com>
2019-02-17 19:08:34 -0800

    transfered to WP

--- wordpress 3/wp-content/themes/minimum-theme/page-about.php
+++ wordpress 3/wp-content/themes/minimum-theme/page-about.php
@@ -10,9 +10,39 @@
 
 <?php get_header()?>
 
-<div id="page-site-wrapper">
+<div id="page-site-wrapper" class="about-only">
 
+    <div class="about-wrapper">
 
+        <div class="about-img">
+            <img src="<?php the_post_thumbnail_url() ?>" alt="">
+        </div>
+
+        <div class="about-dsc">
+
+            <div class="left-dsc">
+                <h4>Shosuke Doi</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
+
+            <div class="right-dsc">
+                <h4>Hobby</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="sns-links">
+        <ul>
+            <li><p><a href="">Instagram</a></p></li>
+            <li><p><a href="">Twitter</a></p></li>
+            <li><p><a href="">Github</a></p></li>
+            <li><p><a href="">LinkedIn</a></p></li>
+            <li><p><a href="">Spotify</a></p></li>
+        </ul>
+    </div>
 
 </div>
 
